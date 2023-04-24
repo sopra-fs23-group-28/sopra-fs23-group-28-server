bf98b01ee1551983f655ce325f7381b0cd730160
samuelfrnk <samuelandreas.frank@uzh.ch>
2023-04-24 17:28:06 +0200

    socket io config update for GC

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
@@ -1,8 +1,6 @@
 package ch.uzh.ifi.hase.soprafs23.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
