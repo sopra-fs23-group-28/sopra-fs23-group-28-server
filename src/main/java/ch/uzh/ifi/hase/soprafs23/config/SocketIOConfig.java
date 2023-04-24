ab5747af84fef94886409b878fe622c490e1bb5a
samuelfrnk <samuelandreas.frank@uzh.ch>
2023-04-24 16:08:15 +0200

    socket io config update for GC

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
@@ -1,15 +1,18 @@
 package ch.uzh.ifi.hase.soprafs23.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SocketIOConfig {
 
-    private String host = "localhost";
-
-    private Integer port = 9092;
+    @Value("${socketio.host:#{systemEnvironment['SOCKETIO_HOST']}}")
+    private String host;
+    @Value("${socketio.port:#{systemEnvironment['SOCKETIO_PORT']}}")
+    private Integer port;
 
     @Bean
     public SocketIOServer socketIOServer() {
