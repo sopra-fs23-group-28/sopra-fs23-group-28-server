bcc75d51664358026b5fb91465f2e083a14ec8bf
Cedric Styner <cstyner@gleclerc.ch>
2023-04-27 17:48:40 +0200

    socket on GCP

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs23.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.Transport;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,15 @@ public class SocketIOConfig {
 
     private Integer port = 65080;
 
+    /**
+     * @return
+     */
     @Bean
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         config.setHostname(host);
         config.setPort(port);
+        config.setOrigin("http://sopra-fs23-group-28-client.oa.r.appspot.com");
         return new SocketIOServer(config);
     }
 }
