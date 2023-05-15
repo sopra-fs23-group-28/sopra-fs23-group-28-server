eccc4ff7220978ed1e65f931b5631637a0334f87
so-ri <so-ri@users.noreply.github.com>
2023-05-15 15:47:49 +0200

    SSL/CORS trial fix 7

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
@@ -1,9 +1,6 @@
 package ch.uzh.ifi.hase.soprafs23.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.Transport;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,7 +21,7 @@ public class SocketIOConfig {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         config.setHostname(host);
         config.setPort(port);
-        config.setOrigin("http://sopra-fs23-group-28-client.oa.r.appspot.com");
+        config.setOrigin("https://sopra-fs23-group-28-client.oa.r.appspot.com");
         return new SocketIOServer(config);
     }
 }
