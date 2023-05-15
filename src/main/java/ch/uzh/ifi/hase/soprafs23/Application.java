e7c3f1da33517d0ecc21be00b3a572a55997a558
so-ri <so-ri@users.noreply.github.com>
2023-05-15 17:54:38 +0200

    reverse SSL trials - forwarding with forwardservlet

--- src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
@@ -1,7 +1,13 @@
 package ch.uzh.ifi.hase.soprafs23;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -37,4 +43,40 @@ public class Application {
             }
         };
     }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+
+                // Add the additional mapping for /socket.io/ to port 65080
+                context.addServletMappingDecoded("/socket.io/*", "forwardToSocketIO");
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(65080);
+        return connector;
+    }
+
+    @Bean(name = "forwardToSocketIO")
+    public ForwardServlet forwardToSocketIO() {
+        return new ForwardServlet("http://localhost:65080");
+    }
+
+
 }
