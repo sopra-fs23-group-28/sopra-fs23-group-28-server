3646803ffa07c14dd061a3218c630e04359bdeb6
samuelfrnk <samuelandreas.frank@uzh.ch>
2023-05-20 11:58:37 +0200

    Ports bugfix

--- src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
@@ -1,10 +1,7 @@
 package ch.uzh.ifi.hase.soprafs23;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -41,37 +37,4 @@ public class Application {
             }
         };
     }
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(org.apache.catalina.Context context) {
-                super.postProcessContext(context);
-                org.apache.catalina.Wrapper wrapper = context.createWrapper();
-                wrapper.setName("forwardToSocketIO");
-                wrapper.setServlet(new ForwardServlet("http://localhost:65080"));
-                context.addChild(wrapper);
-                context.addServletMappingDecoded("/socket.io/*", "forwardToSocketIO");
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
-
 }
