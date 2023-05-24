8acf943e37da98a9ac7b4da7278037847bf1652c
so-ri <so-ri@users.noreply.github.com>
2023-05-24 18:37:00 +0200

    removed origin

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/SocketIOConfig.java
@@ -15,7 +15,6 @@ public class SocketIOConfig {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         config.setHostname(host);
         config.setPort(port);
-        config.setOrigin("https://sopra-fs23-group-28-client.oa.r.appspot.com");
         return new SocketIOServer(config);
     }
 }
